3480d88a2dee5873492422c556b6aa6e05d2194b
Bingxing Wang <i@imbushuo.net>
2018-04-24 11:41:19 -0700

    Fix problems on Lumia 930.
    Windows Phone Blue devices do not support SDHCI HS400, target configuration has been modified to adapt this.

--- target/msm8974/init.c
+++ target/msm8974/init.c
@@ -230,6 +230,10 @@ static void target_mmc_sdhci_init(void)
 			else
 				config.bus_width = DATA_BUS_WIDTH_8BIT;
 			break;
+		case HW_PLATFORM_CHI:
+			/* WPBlue devices do not have 8bit bus */
+			config.bus_width = DATA_BUS_WIDTH_4BIT;
+			break;
 		default:
 			config.bus_width = DATA_BUS_WIDTH_8BIT;
 	};
@@ -252,19 +256,11 @@ static void target_mmc_sdhci_init(void)
 	config.sdhc_base = mmc_sdhci_base[config.slot - 1];
 	config.pwrctl_base = mmc_sdc_base[config.slot - 1];
 	config.pwr_irq     = mmc_sdc_pwrctl_irq[config.slot - 1];
+	config.hs200_support = 1;
 
 	if (!(dev = mmc_init(&config))) {
-		/* Trying Slot 2 next */
-		config.slot = 2;
-		config.max_clk_rate = MMC_CLK_200MHZ;
-		config.sdhc_base = mmc_sdhci_base[config.slot - 1];
-		config.pwrctl_base = mmc_sdc_base[config.slot - 1];
-		config.pwr_irq     = mmc_sdc_pwrctl_irq[config.slot - 1];
-
-		if (!(dev = mmc_init(&config))) {
-			dprintf(CRITICAL, "mmc init failed!");
-			ASSERT(0);
-		}
+		dprintf(CRITICAL, "mmc init failed!");
+		ASSERT(0);
 	}
 
 	/*
